95ea085298156a326a3be0231709087a926336d2
Twixii <lilkamal76@gmail.com>
2020-04-29 02:12:18 +0200

    All tests are ok

--- src/eg/edu/alexu/csd/filestructure/btree/IBTreeDisplay.java
+++
@@ -1,5 +0,0 @@
-package eg.edu.alexu.csd.filestructure.btree;
-
-public interface IBTreeDisplay {
-    public void B_Tree_Display(IBTreeNodeExtended root);
-}
--- src/eg/edu/alexu/csd/filestructure/btree/Main.java
+++ src/eg/edu/alexu/csd/filestructure/btree/Main.java
@@ -9,17 +9,13 @@ public class Main {
 
         Integer[] arr = {1, 3, 7, 10, 11, 13, 14, 15, 18, 16, 19, 24, 25, 26, 21, 4, 5, 20, 22, 2, 17, 12, 6};
         Integer[] arr2 = {3, 19, 15, 6, 20, 26, 14, 21, 13, 25, 1, 7, 5, 16, 4, 11, 18, 22, 10, 2, 12, 24, 17};
-        System.out.println(arr.length);
+
         for (Integer x : arr) {
             ibTree.insert(x, "Soso" + x);
         }
 
         for(Integer x : arr2) {
-            System.out.print(ibTree.search(x));
-            System.out.print("------------>>>>>");
-            System.out.print(ibTree.delete(x));
-            System.out.print("------------>>>>>");
-            System.out.println(ibTree.search(x));
+            System.out.println(ibTree.delete(x));
         }
     }
 }
